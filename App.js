a52f167ec6ce719e2283e3f722f58aa28f194246
William Grant <willmgrant@gmail.com>
2018-09-08 18:49:14 +1000

    Connected to ChatKit, pre made messages are now logged to console.

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import React from 'react'
+import ChatKit from '@pusher/chatkit'
 import MessageList from './components/MessageList'
 import SendMessageForm from './components/SendMessageForm'
 import RoomList from './components/RoomList'
 import NewRoomForm from './components/NewRoomForm'
 
+import {tokenUrl, instanceLocator} from './config'
+
 class App extends React.Component {
+    componentDidMount() { {/*Triggered directly after render() has been invoked*/}
+    {/* dont need instanceLocator: instanceLocator because they have the same name */}
+      const chatManager = new ChatKit.ChatManager({
+        instanceLocator,
+        userId: 'yougotwill',
+        tokenProvider: new ChatKit.TokenProvider({
+            url: tokenUrl
+        })
+      })
+
+      chatManager.connect().then(currentUser => { {/* currentUser is the interface to communicate with the ChatKit API */}
+        currentUser.subscribeToRoom({
+            roomId: 15686974,
+            hooks: {
+                onNewMessage: message => {
+                    console.log('message.text', message.text)
+                }
+            }
+        })
+      })
+    }
+
     render() {
         return (
             <div className="app">
--- config.js
+++ config.js
@@ -1,6 +1,5 @@
-const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/af799227-05d3-4a5b-ae27-8e378f5683b1/token";
-const instanceLocator = "v1:us1:af799227-05d3-4a5b-ae27-8e378f5683b1";
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/86cb4a62-c266-4cfd-badf-17308db2c05f/token";
+const instanceLocator = "v1:us1:86cb4a62-c266-4cfd-badf-17308db2c05f";
 
-exports.tokenUrl = tokenUrl;
-exports.instanceLocator = instanceLocator;
+export {tokenUrl, instanceLocator}
 
